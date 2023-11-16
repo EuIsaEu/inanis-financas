2b080291f6394cd28d229589e9c4c2c0cdc60eb3
Isa <87494452+EuIsaEu@users.noreply.github.com>
2023-11-16 18:26:10 +0000

    criando dicas

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 
-const licaoDinheiro = document.getElementById('licao-dinheiro');
-const logoInsta = document.getElementById('insta-logo');
 
-licaoDinheiro.addEventListener("mouseover", (event) => {
-    logoInsta.dataset.opacity = "1";
-});
+var home = () => {
 
-licaoDinheiro.addEventListener("mouseout", (event) => {
-    logoInsta.dataset.opacity = "0";
-});
-
-licaoDinheiro.addEventListener("click", (event) => {
-    window.open("https://www.instagram.com/projetomilhao/", '_blank')
-})
+    const licaoDinheiro = document.getElementById('licao-dinheiro');
+    const logoInsta = document.getElementById('insta-logo');
+    const primoRicoImg = document.getElementById('primo-rico-img');
+    
+    licaoDinheiro.addEventListener("mouseover", (event) => {
+        logoInsta.dataset.opacity = "1";
+    });
+    
+    licaoDinheiro.addEventListener("mouseout", (event) => {
+        logoInsta.dataset.opacity = "0";
+    });
+    
+    primoRicoImg.addEventListener("click", (event) => {
+        window.open("https://youtu.be/in0XbfQEm2A", '_blank')
+    })
+}
